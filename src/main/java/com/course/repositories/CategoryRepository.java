c926970e3a07e7856ac3db3a758236ff69520ff3
Gabriela Cordova <gabrielacordovaa@hotmail.com>
2020-05-19 17:03:54 -0300

    Category

---
+++ src/main/java/com/course/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.course.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
